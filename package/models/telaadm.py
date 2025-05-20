6afb0895c16cde06706f2310cfa011f2a43bf10d
Beatriz Brandão <beatrizbfb@icloud.com>
2025-05-20 09:49:42 -0300

    Correção

--- package/models/telaadm.py
+++ package/models/telaadm.py
@@ -13,39 +13,25 @@ class TelaAdm:
     def menu(self):
         while True:
             print("\n======== MENU DO ADMINISTRADOR ========")
-            print("1. Listar clientes")
+            print("1. Mostrar clientes")
             print("2. Acessar estoque")
             print("3. Confirmar entrega para um cliente")
-            print("4. Mostrar página dos clientes cadastrados")
-            print("5. Sair")
+            print("4. Sair")
 
             escolha = input("Escolha: ")
 
             if escolha == "1":
-                self.listar_clientes()
+                self.mostrar_clientes_html()
             elif escolha == "2":
                 self.estoque.menu()
             elif escolha == "3":
                 self.confirmar_entrega()
             elif escolha == "4":
-                self.mostrar_clientes_html()
-            elif escolha == "5":
                 print("Saindo da área do administrador...")
                 sleep(1)
                 break
             else:
                 print("Opção inválida.")
-
-
-    def listar_clientes(self):
-        print("\n=== LISTA DE CLIENTES ===")
-        clientes = self.clientes.read()
-        if not clientes:
-            print("Nenhum cliente cadastrado.")
-        else:
-            for i, cliente in enumerate(clientes):
-                print(f"{i + 1}. Nome: {cliente['nome']}")
-        
     
     def confirmar_entrega(self):
         print("\n=== CONFIRMAR ENTREGA ===")
